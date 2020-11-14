ebf623abe0b4b7d08c544a1e7e078b378d83c19c
Gabriel Moncau <gabriel.moncau@hotmail.com>
2020-11-14 14:52:43 -0500

    Working Google Sheets link

---
+++ BostonHacks 2020/Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using GoogleSheetsToUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [HideInInspector]
+    public string associatedSheet = "1r7oUGjUpapdhlDBwxJnwajmHPzfIwgjMKl4rvHHPgLc";
+    [HideInInspector]
+    public string associatedWorksheet = "Data";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpreadsheetManager.Read(new GSTU_Search(associatedSheet, associatedWorksheet), UpdateData);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateData(GstuSpreadSheet ss)
+    {
+        Debug.Log(ss["B2"].value);
+    }
+}
